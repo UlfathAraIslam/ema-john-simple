4f03e6b3812eea016db01361d10b44338cce97ce
pc <pc@pishinoiMac.local>
2023-04-08 10:21:59 +0900

    react router is used for cart, shop, orders component

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,7 @@ import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import'./Shop.css';
+import { Link } from 'react-router-dom';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
@@ -54,6 +55,10 @@ const Shop = () => {
         addToDb(product.id)
 
     };
+    const handleClearCart = ()=> {
+        setCart([]);
+        deleteShoppingCart();
+    }
 
     return (
         <div className='shop-container'>
@@ -67,7 +72,17 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart= {cart}></Cart>
+                <Cart 
+                cart= {cart}
+                handleClearCart={handleClearCart}
+
+                >
+
+                <Link className='proceed-link' to="/orders">
+                    <button className='btn-proceed'>Review Order</button>
+                </Link>
+
+                </Cart>
             </div>
         </div>
     );
